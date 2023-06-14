cf3122ac6fdf474690a08029c5a22b7e141e7d20
Tom Mount <tmountjr@gmail.com>
2023-06-14 18:32:17 -0400

    get bounding boxes set up for dynamic content

--- src/main.cpp
+++ src/main.cpp
@@ -20,8 +20,23 @@ void setup()
     // Just write out shit hardcoded for now
     display.fillScreen(0xFFFF);
     display.setFont(&FreeSans9pt7b);
-    display.setCursor(0, 17);
-    display.println("Today is 06/14/2023");
+    int16_t boundX, boundY;
+    uint16_t boundW, boundH;
+
+    char TodayIs[] = "Today is ";
+    display.getTextBounds(TodayIs, 0, 0, &boundX, &boundY, &boundW, &boundH);
+
+    display.setCursor(0, -boundY + 5);
+    display.print(TodayIs);
+    int today_is_x = display.getCursorX();
+    // The (x,y) cursor is at the bottom left of where the next bit of text would appear.
+    // X is correct but Y needs to set to -boundY + 5
+    display.drawRect(today_is_x, 5, display.width() - today_is_x - 1, boundH, 0x0000);
+
+    // Testing
+    // display.setCursor(today_is_x, -boundY + 5);
+    // display.print("06/03/2023");
+
     int16_t line_y = 25;
     int16_t line_x = (display.width() / 2) - 1;
     for (int i = 0; i < 3; i++)
@@ -31,10 +46,9 @@ void setup()
       line_y += 1;
       line_x += 1;
     }
-    
+
     line_y = 45;
-    int16_t boundX, boundY;
-    uint16_t boundW, boundH;
+    line_x -= 1;
 
     // Current
     display.setCursor(0, line_y);
@@ -43,7 +57,6 @@ void setup()
 
     // Next
     uint16_t x_start = line_x + 1; // because exact midpoint is a line, and another line next to it (+1), and then things start (+1)
-    Serial.printf("x_start: %d\n", x_start);
     char UpNext[] = "Up Next:";
     display.getTextBounds(UpNext, x_start, 45, &boundX, &boundY, &boundW, &boundH);
     // 3/4 point of the screen = display.width() * 0.75
@@ -53,13 +66,23 @@ void setup()
     display.setCursor(text_x, 45);
     display.print("Up Next:");
     
-    int16_t cursor_x = display.getCursorX();
     int16_t cursor_y = display.getCursorY();
-    Serial.printf("cursorX: %d, cursor_y: %d\n", cursor_x, cursor_y);
 
     // Draw some rectangles to define dynamic areas
-    display.drawRect(5, cursor_y + 10, 136, 68, 0x0000);
-    display.drawRect(154, cursor_y + 10, 136, 68, 0x0000);
+    /**
+     * So we need to calculate the following:
+     *   * left rect x is always 5 (or whatever the padding we want is, in this case 5)
+     *   * right rect x is midpoint (148) + 1 (extra line) + 5 (padding) + 1 (algebra)
+     *   * left rect y and right rect y are always cursor_y + 10 (arbitrary padding to allow for under-baseline stuff)
+     *   * width (136) is calculated as...display.width() [296] / 2 [148] - 10 [2x padding, 138] - 1 [extra line, 137] - 1 [algebra, 136]
+     *   * height (68) is calculated as...display.height() [128] - (cursor_y + 10) [55, so 73] - 5 [bottom padding, 68]
+     */
+    int16_t rect_width = (display.width() / 2) - 12;
+    int16_t rect_height = display.height() - (cursor_y + 10) - 5;
+    int16_t right_rect_x = (display.width() / 2) + 7;
+    display.drawRect(5, cursor_y + 10, rect_width, rect_height, 0x0000);  // left rectangle
+    display.drawRect(right_rect_x, cursor_y + 10, rect_width, rect_height, 0x0000); // right rectangle
+    
 
     // TODO: remove date and add in box outlining date area
   } while (display.nextPage());
